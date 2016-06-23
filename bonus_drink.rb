a062ed854715e83698567f03301c6fed5c7e3b10
Kenichi Hasegawa <kenichi.hasegawa.1969@gmail.com>
2016-06-23 15:48:07 +0900

    単体テスト追加 戻り値修正

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
+
   def self.total_count_for(amount)
-    0
+    if not amount.is_a?(Integer) or (amount < 0) then
+      print("amountは0以上の数値で指定してください 指定された値：", amount,"\n")
+      return
+    end
+
+    num = amount
+    total = amount
+    while num >= 3 do
+      num = num - 3
+      num = num + 1
+      total = total + 1
+    end
+    print("購入本数：", amount, " 飲める本数：",total,"\n")
+    return total
   end
+
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(-1)).to eq nil }
+  specify { expect(BonusDrink.total_count_for('a')).to eq nil }
+
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(10)).to eq 14 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
 end
